58089cb17db238dabb2611f2b0c47618eb65d405
Andreas Waigand <awaigand@gmx.net>
2015-05-13 14:31:07 +0200

    Add text search for books. Add tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,28 @@
 class SearchController < ApplicationController
 
+  # From https://github.com/zapnap/isbn_validation/blob/master/lib/isbn_validation.rb
+  ISBN10_REGEX = /^(?:\d[\ |-]?){9}[\d|X]$/i
+  ISBN13_REGEX = /^(?:\d[\ |-]?){13}$/i
+
   def search
   end
 
   def query
-    books = GoogleBooks.search("isbn:#{params[:isbn]}", {:count => 10})
-    @books = books
+      books_param = params[:search]
+      flash[:message] = "Those are the results for the text you entered. Add?"
+      if check_isbn(params[:search])
+        flash[:message]= "You entered the ISBN of this book. Do you want to add it to your library?"
+        books_param="isbn:#{params[:search]}"
+      end
+      @books = GoogleBooks.search(books_param, {:count => 10})
   end
 
+private
 
+  def check_isbn(isbn)
+    # Note that this does not actually check if it is a VALID ISBN (checksum)
+    # It only checks whether it is formatted like an ISBN
+    isbn.match(ISBN10_REGEX) || isbn.match(ISBN13_REGEX)
+  end
 
 end
--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -6,4 +6,23 @@ class SearchControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should recognize isbn-10 in search' do
+    post :query, search: '3453146972'
+    assert_response :ok
+    assert_select '#flash', "You entered the ISBN of this book. Do you want to add it to your library?"
+  end
+
+  test 'should recognize isbn-13 in search' do
+    post :query, search: '978-3453146972'
+    assert_response :ok
+    assert_select '#flash', "You entered the ISBN of this book. Do you want to add it to your library?"
+  end
+
+  test 'should recognize normal text in search' do
+    post :query, search: 'Test Author'
+    assert_response :ok
+    assert_select '#flash', "Those are the results for the text you entered. Add?"
+  end
+
+
 end
