05a7787ced1b46fa1c2efed6bfcb8fa4c9ba2619
Andreas Waigand <awaigand@gmx.net>
2015-05-15 12:35:54 +0200

    Add for_author scope to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ActiveRecord::Base
   belongs_to :lender
   has_and_belongs_to_many :authors
   validates_presence_of :title
+
+  scope :for_author, -> (author){ joins(:authors).where(:authors => {:name => author})}
+  
+  before_destroy{ books.clear }
 end
